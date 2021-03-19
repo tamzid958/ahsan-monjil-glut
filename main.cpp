c12dc72f3992cb1504b30e033aa26193165c3c54
Tamzid Ahmed <tamjidahmed958@gmail.com>
2021-03-19 16:32:41 +0600

    ahsan monjil body created

--- main.cpp
+++ main.cpp
@@ -1,176 +1,453 @@
-/*
- * GLUT Shapes Demo
- *
- * Written by Nigel Stewart November 2003
- *
- * This program is test harness for the sphere, cone
- * and torus shapes in GLUT.
- *
- * Spinning wireframe and smooth shaded shapes are
- * displayed until the ESC or q key is pressed.  The
- * number of geometry stacks and slices can be adjusted
- * using the + and - keys.
- */
-
-#ifdef __APPLE__
-#include <GLUT/glut.h>
-#else
+#include <windows.h>
+#include <iostream>
 #include <GL/glut.h>
-#endif
-#include <stdlib.h>
+#include <math.h>
+#define PI 3.1416
 
-static int slices = 16;
-static int stacks = 16;
+int i;
+int triangleAmount = 100;
+GLfloat rotation_increaser = 0.0f;
+GLfloat position = 0.0f;
+GLfloat speed = 0.1f;
+GLfloat x = 0,y = 0,radius = 0;
+GLfloat twicePi = 2.0f * PI;
 
-/* GLUT callback Handlers */
+void update(int value) {
 
-static void resize(int width, int height)
+    if(position <-1.0)
+        position = 1.0f;
+
+    position -= speed;
+
+	glutPostRedisplay();
+
+
+	glutTimerFunc(100, update, 0);
+}
+
+void Idle()
 {
-    const float ar = (float) width / (float) height;
+    glutPostRedisplay();
+}
 
-    glViewport(0, 0, width, height);
-    glMatrixMode(GL_PROJECTION);
+void initState(){
+    glClearColor(0.5f, 1.0f, 1.0f, 0.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glLineWidth(4);
+    glPointSize(7);
+}
+
+void axisDraw(){
+    glBegin(GL_LINES);
+    glColor3ub(0, 0, 0);
+    glVertex2f(1.0f, 0.0f);
+    glVertex2f(-1.0, 0.0f);
+    glVertex2f(0.0f, 1.0f);
+    glVertex2f(0.0, -1.0f);
+    glEnd();
+}
+void pillarComponent(){
+
+    glBegin(GL_QUADS);
+    glColor3ub(255, 69, 0);
+    glVertex2f(-0.75f, 0.5f);
+    glVertex2f(-0.75f, 0.1f);
+    glVertex2f(-0.8f, 0.1f);
+    glVertex2f(-0.8f, 0.5f);
+    glEnd();
+
+
+    glBegin(GL_QUADS);
+    glColor3ub(255, 69, 0);
+    glVertex2f(-0.755f, 0.55f);
+    glVertex2f(-0.755f, 0.5f);
+    glVertex2f(-0.795f, 0.5f);
+    glVertex2f(-0.795f, 0.55f);
+    glEnd();
+
+
+    x = -.775f; y = .55f; radius =.02f;
+	glBegin(GL_TRIANGLE_FAN);
+        glColor3ub(255, 69, 0);
+		glVertex2f(x, y);
+		for(i = 0; i <= triangleAmount;i++) {
+			glVertex2f(
+		            x + (radius * cos(i *  twicePi / triangleAmount)),
+			    y + (radius * sin(i * twicePi / triangleAmount))
+			);
+		}
+	glEnd();
+
+	glLineWidth(4);
+	glBegin(GL_LINES);
+	glColor3ub(255, 69, 0);
+	glVertex2f(-0.776f, 0.59f);
+    glVertex2f(-0.776f, 0.56f);
+    glVertex2f(-0.785f, 0.58f);
+    glVertex2f(-0.77f, 0.58f);
+	glEnd();
+}
+
+void gombujhComponent(){
+  glBegin(GL_QUADS);
+  glColor3ub(255, 69, 0);
+  glVertex2f(-0.6f, 0.57f);
+  glVertex2f(-0.6f, 0.52f);
+  glVertex2f(-0.75f, 0.52f);
+  glVertex2f(-0.75f, 0.57f);
+  glEnd();
+
+   x = -.68f; y = .56f; radius =.045f;
+   glBegin(GL_TRIANGLE_FAN);
+        glColor3ub(255, 69, 0);
+		glVertex2f(x, y);
+		for(i = 0; i <= triangleAmount;i++) {
+			glVertex2f(
+		            x + (radius * cos(i *  twicePi / triangleAmount)),
+			    y + (radius * sin(i * twicePi / triangleAmount))
+			);
+		}
+
+	glEnd();
+
+
+   glBegin(GL_POINTS);
+   glColor3ub(255, 69, 0);
+   glVertex2f(-0.68f, 0.62f);
+   glEnd();
+}
+
+void squareWindowComponent(){
+    glBegin(GL_QUADS);
+    glColor3ub(178, 34, 34);
+    glVertex2f(-0.69f, 0.4f);
+    glVertex2f(-0.69f, 0.3f);
+    glVertex2f(-0.73f, 0.3f);
+    glVertex2f(-0.73f, 0.4f);
+    glEnd();
+}
+
+void ovalWindowComponent(int R, int G, int B){
+
+    glBegin(GL_QUADS);
+    glColor3ub(R, G, B);
+    glVertex2f(-0.69f, 0.4f);
+    glVertex2f(-0.69f, 0.3f);
+    glVertex2f(-0.73f, 0.3f);
+    glVertex2f(-0.73f, 0.4f);
+    glEnd();
+
+    x = -.71f; y = .4f; radius =.021f;
+	glBegin(GL_TRIANGLE_FAN);
+        glColor3ub(R, G, B);
+		glVertex2f(x, y);
+		for(i = 0; i <= triangleAmount;i++) {
+			glVertex2f(
+		            x + (radius * cos(i *  twicePi / triangleAmount)),
+			    y + (radius * sin(i * twicePi / triangleAmount))
+			);
+		}
+	glEnd();
+}
+
+
+void threeDotComponent(){
+   glBegin(GL_POINTS);
+   glColor3ub(0, 0, 0);
+   glVertex2f(-0.68f, 0.62f);
+   glVertex2f(-0.66f, 0.62f);
+   glVertex2f(-0.64f, 0.62f);
+   glEnd();
+}
+
+void ahsanMonjilView(){
+
+    glBegin(GL_QUADS);
+    glColor3ub(255, 99, 71);
+    glVertex2f(0.8f,0.5f);
+    glVertex2f(0.8f,0.1f);
+    glVertex2f(-0.8f,0.1f);
+    glVertex2f(-0.8f,0.5f);
+    glEnd();
+
+    glBegin(GL_LINES);
+    glColor3ub(255, 99, 71);
+    glVertex2f(-0.81f, 0.51f);
+    glVertex2f(0.81f, 0.51f);
+    glEnd();
+
+
+    glLineWidth(10);
+    glBegin(GL_LINES);
+    glColor3ub(255, 99, 71);
+    glVertex2f(-0.55f, 0.53f);
+    glVertex2f(0.6f, 0.53f);
+    glEnd();
+
     glLoadIdentity();
-    glFrustum(-ar, ar, -1.0, 1.0, 2.0, 100.0);
+    glTranslatef(0.15, -0.09, 0);
+    threeDotComponent();
+    glLoadIdentity();
 
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity() ;
+    glLoadIdentity();
+    glTranslatef(0.25, -0.09, 0);
+    threeDotComponent();
+    glLoadIdentity();
+
+    glLoadIdentity();
+    glTranslatef(0.35, -0.09, 0);
+    threeDotComponent();
+    glLoadIdentity();
+
+    glLoadIdentity();
+    glTranslatef(0.45, -0.09, 0);
+    threeDotComponent();
+    glLoadIdentity();
+
+    glLoadIdentity();
+    glTranslatef(0.55, -0.09, 0);
+    threeDotComponent();
+    glLoadIdentity();
+
+
+    glLoadIdentity();
+    glTranslatef(0.82, -0.09, 0);
+    threeDotComponent();
+    glLoadIdentity();
+
+    glLoadIdentity();
+    glTranslatef(0.92, -0.09, 0);
+    threeDotComponent();
+    glLoadIdentity();
+
+    glLoadIdentity();
+    glTranslatef(1.02, -0.09, 0);
+    threeDotComponent();
+    glLoadIdentity();
+
+    glLoadIdentity();
+    glTranslatef(1.12, -0.09, 0);
+    threeDotComponent();
+    glLoadIdentity();
+
+    glLoadIdentity();
+    glTranslatef(1.22, -0.09, 0);
+    threeDotComponent();
+    glLoadIdentity();
+
+    glLoadIdentity();
+    pillarComponent();
+    glLoadIdentity();
+
+    glLoadIdentity();
+    glTranslatef(0.2, 0, 0);
+    pillarComponent();
+    glLoadIdentity();
+
+    glLoadIdentity();
+    glTranslatef(1.4, 0, 0);
+    pillarComponent();
+    glLoadIdentity();
+
+    glLoadIdentity();
+    glTranslatef(1.6, 0, 0);
+    pillarComponent();
+    glLoadIdentity();
+
+    glLoadIdentity();
+    gombujhComponent();
+    glLoadIdentity();
+
+    glLoadIdentity();
+    glTranslatef(1.4, 0, 0);
+    gombujhComponent();
+    glLoadIdentity();
+
+    glLoadIdentity();
+    squareWindowComponent();
+    glLoadIdentity();
+
+//left windows
+    glLoadIdentity();
+    squareWindowComponent();
+    glLoadIdentity();
+
+    glLoadIdentity();
+    glTranslatef(0.07, 0, 0);
+    squareWindowComponent();
+    glLoadIdentity();
+
+    glLoadIdentity();
+    glTranslatef(0, -0.15, 0);
+    ovalWindowComponent(178, 34, 34);
+    glLoadIdentity();
+
+    glLoadIdentity();
+    glTranslatef(0.07, -0.15, 0);
+    ovalWindowComponent(178, 34, 34);
+    glLoadIdentity();
+
+//right windows
+    glLoadIdentity();
+    glTranslatef(1.4, 0, 0);
+    squareWindowComponent();
+    glLoadIdentity();
+
+    glLoadIdentity();
+    glTranslatef(1.47, 0, 0);
+    squareWindowComponent();
+    glLoadIdentity();
+
+    glLoadIdentity();
+    glTranslatef(1.4, -0.15, 0);
+    ovalWindowComponent(178, 34, 34);
+    glLoadIdentity();
+
+    glLoadIdentity();
+    glTranslatef(1.47, -0.15, 0);
+    ovalWindowComponent(178, 34, 34);
+    glLoadIdentity();
+
+//bottom row of windows (left)
+    glLoadIdentity();
+    glTranslatef(0.2, -0.2, 0);
+    ovalWindowComponent(0, 0, 0);
+    glLoadIdentity();
+
+    glLoadIdentity();
+    glTranslatef(0.3, -0.2, 0);
+    ovalWindowComponent(0, 0, 0);
+    glLoadIdentity();
+
+    glLoadIdentity();
+    glTranslatef(0.4, -0.2, 0);
+    ovalWindowComponent(0, 0, 0);
+    glLoadIdentity();
+
+    glLoadIdentity();
+    glTranslatef(0.5, -0.2, 0);
+    ovalWindowComponent(0, 0, 0);
+    glLoadIdentity();
+
+//upper row of windows (left)
+    glLoadIdentity();
+    glTranslatef(0.2, 0, 0);
+    ovalWindowComponent(0, 0, 0);
+    glLoadIdentity();
+
+    glLoadIdentity();
+    glTranslatef(0.3, 0, 0);
+    ovalWindowComponent(0, 0, 0);
+    glLoadIdentity();
+
+    glLoadIdentity();
+    glTranslatef(0.4, 0, 0);
+    ovalWindowComponent(0, 0, 0);
+    glLoadIdentity();
+
+    glLoadIdentity();
+    glTranslatef(0.5, 0, 0);
+    ovalWindowComponent(0, 0, 0);
+    glLoadIdentity();
+
+    glLoadIdentity();
+    glTranslatef(0.6, 0, 0);
+    ovalWindowComponent(0, 0, 0);
+    glLoadIdentity();
+
+
+//bottom row of windows (right)
+    glLoadIdentity();
+    glTranslatef(1.27, -0.2, 0);
+    ovalWindowComponent(0, 0, 0);
+    glLoadIdentity();
+
+    glLoadIdentity();
+    glTranslatef(1.17, -0.2, 0);
+    ovalWindowComponent(0, 0, 0);
+    glLoadIdentity();
+
+    glLoadIdentity();
+    glTranslatef(1.07, -0.2, 0);
+    ovalWindowComponent(0, 0, 0);
+    glLoadIdentity();
+
+    glLoadIdentity();
+    glTranslatef(0.97, -0.2, 0);
+    ovalWindowComponent(0, 0, 0);
+    glLoadIdentity();
+
+
+//upper row of windows (right)
+    glLoadIdentity();
+    glTranslatef(1.27, 0, 0);
+    ovalWindowComponent(0, 0, 0);
+    glLoadIdentity();
+
+    glLoadIdentity();
+    glTranslatef(1.17, 0, 0);
+    ovalWindowComponent(0, 0, 0);
+    glLoadIdentity();
+
+    glLoadIdentity();
+    glTranslatef(1.07, 0, 0);
+    ovalWindowComponent(0, 0, 0);
+    glLoadIdentity();
+
+    glLoadIdentity();
+    glTranslatef(0.97, 0, 0);
+    ovalWindowComponent(0, 0, 0);
+    glLoadIdentity();
+
+    glLoadIdentity();
+    glTranslatef(0.87, 0, 0);
+    ovalWindowComponent(0, 0, 0);
+    glLoadIdentity();
+
+//stairs
+    glLineWidth(7);
+    glBegin(GL_LINES);
+    glColor3ub(35,43,43);
+    glVertex2f(0.17f, 0.3f);
+    glVertex2f(-0.12f, 0.3f);
+
+    glVertex2f(0.2f, 0.25f);
+    glVertex2f(-0.15f, 0.25f);
+
+    glVertex2f(0.23f, 0.20f);
+    glVertex2f(-0.18f, 0.20f);
+
+    glVertex2f(0.26f, 0.15f);
+    glVertex2f(-0.21f, 0.15f);
+
+    glVertex2f(0.29f, 0.10f);
+    glVertex2f(-0.24f, 0.10f);
+
+    glVertex2f(0.32f, 0.05f);
+    glVertex2f(-0.27f, 0.05f);
+
+    glVertex2f(0.35f, 0.0f);
+    glVertex2f(-0.3f, 0.0f);
+    glEnd();
+
+
 }
 
-static void display(void)
-{
-    const double t = glutGet(GLUT_ELAPSED_TIME) / 1000.0;
-    const double a = t*90.0;
-
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glColor3d(1,0,0);
-
-    glPushMatrix();
-        glTranslated(-2.4,1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutSolidSphere(1,slices,stacks);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslated(0,1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutSolidCone(1,1,slices,stacks);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslated(2.4,1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutSolidTorus(0.2,0.8,slices,stacks);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslated(-2.4,-1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutWireSphere(1,slices,stacks);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslated(0,-1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutWireCone(1,1,slices,stacks);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslated(2.4,-1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutWireTorus(0.2,0.8,slices,stacks);
-    glPopMatrix();
-
-    glutSwapBuffers();
+void fullView(){
+   initState();
+   axisDraw();
+   ahsanMonjilView();
+   glFlush();
 }
 
 
-static void key(unsigned char key, int x, int y)
-{
-    switch (key)
-    {
-        case 27 :
-        case 'q':
-            exit(0);
-            break;
-
-        case '+':
-            slices++;
-            stacks++;
-            break;
-
-        case '-':
-            if (slices>3 && stacks>3)
-            {
-                slices--;
-                stacks--;
-            }
-            break;
-    }
-
-    glutPostRedisplay();
-}
-
-static void idle(void)
-{
-    glutPostRedisplay();
-}
-
-const GLfloat light_ambient[]  = { 0.0f, 0.0f, 0.0f, 1.0f };
-const GLfloat light_diffuse[]  = { 1.0f, 1.0f, 1.0f, 1.0f };
-const GLfloat light_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
-const GLfloat light_position[] = { 2.0f, 5.0f, 5.0f, 0.0f };
-
-const GLfloat mat_ambient[]    = { 0.7f, 0.7f, 0.7f, 1.0f };
-const GLfloat mat_diffuse[]    = { 0.8f, 0.8f, 0.8f, 1.0f };
-const GLfloat mat_specular[]   = { 1.0f, 1.0f, 1.0f, 1.0f };
-const GLfloat high_shininess[] = { 100.0f };
-
-/* Program entry point */
-
-int main(int argc, char *argv[])
-{
+int main(int argc, char** argv) {
     glutInit(&argc, argv);
-    glutInitWindowSize(640,480);
-    glutInitWindowPosition(10,10);
-    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
-
-    glutCreateWindow("GLUT Shapes");
-
-    glutReshapeFunc(resize);
-    glutDisplayFunc(display);
-    glutKeyboardFunc(key);
-    glutIdleFunc(idle);
-
-    glClearColor(1,1,1,1);
-    glEnable(GL_CULL_FACE);
-    glCullFace(GL_BACK);
-
-    glEnable(GL_DEPTH_TEST);
-    glDepthFunc(GL_LESS);
-
-    glEnable(GL_LIGHT0);
-    glEnable(GL_NORMALIZE);
-    glEnable(GL_COLOR_MATERIAL);
-    glEnable(GL_LIGHTING);
-
-    glLightfv(GL_LIGHT0, GL_AMBIENT,  light_ambient);
-    glLightfv(GL_LIGHT0, GL_DIFFUSE,  light_diffuse);
-    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
-    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
-
-    glMaterialfv(GL_FRONT, GL_AMBIENT,   mat_ambient);
-    glMaterialfv(GL_FRONT, GL_DIFFUSE,   mat_diffuse);
-    glMaterialfv(GL_FRONT, GL_SPECULAR,  mat_specular);
-    glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess);
-
+    glutInitWindowSize(1200, 800);
+    glutCreateWindow("Ahsan Monjil");
+    glutDisplayFunc(fullView);
+    glutIdleFunc(Idle);
+    glutTimerFunc(100, update, 0);
     glutMainLoop();
-
-    return EXIT_SUCCESS;
+    return 0;
 }
